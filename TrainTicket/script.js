5bc519c5b9305f437f6baa90ea7e9aaf493eaacc
Sadman Al Sakib <150785896+saadmanshakiib@users.noreply.github.com>
2025-06-04 01:49:26 +0600

    Add files via upload

--- TrainTicket/script.js
+++ TrainTicket/script.js
@@ -66,6 +66,15 @@ function check(){
                     document.getElementById("birthmont").value = "";
 
                 }
+                else if(users[i].Email != email){
+                    if(enterEmail.endsWith("@gmail.com") && number.length == 11 && year<=2025 && nid.length == 16 && date<=31){
+                        alert("Account has been created!");
+                        window.location.href="loginpage.html";
+                    }
+                    else{
+                        alert("Check all the fields correctly and fullfill");
+                    }
+                }
                     }   
             }
     else{
@@ -78,4 +87,71 @@ function check(){
                     document.getElementById("birthmont").value = "";
     }
     }
+}
+
+function search(){
+
+    const from = document.getElementById("fromStation").value;
+    const to = document.getElementById("toStation").value;
+    const date = document.getElementById("journeyDate").value;
+
+    if(from == "" || to == "" || date == ""){
+        alert("Please fill all the fields");
+    }
+
+        else{
+            window.location.href = "searchResults.html";
+             let trains = [
+        {
+            train_name : "Suborno Express",
+            train_no : "701",
+            from_station : "Chittagong",
+            to_station : "Dhaka",
+            starting_time : "7:30 AM",
+            reaching_time : "12:00 PM"
+        },
+        {
+            train_name : "Sonar Bangla Express",
+            train_no : "702",
+            from_station : "Chittagong",
+            to_station : "Dhaka",
+            starting_time : "5:00 PM",
+            reaching_time : "9:00 PM"
+        },
+        {
+            train_name : "Sonar Bangla Express",
+            train_no : "703",
+            from_station : "Dhaka",
+            to_station : "Chittagong",
+            starting_time : "7:30 AM",
+            reaching_time : "12:00 PM"
+        },
+        {
+            train_name : "Suborno Express",
+            train_no : "704",
+            from_station : "Dhaka",
+            to_station : "Chittagong",
+            starting_time : "7:30 AM",
+            reaching_time : "12:00 PM"
+        }
+    ]
+
+    let matched_trains = trains.filter(
+                                train => train.from_station.toLowerCase() == from.toLowerCase() &&
+                                train.to.toLowerCase() == to.toLowerCase());
+
+                document.getElementById("trainName").value = matched_trains.train_name;    
+                document.getElementById("From").value = matched_trains.from_station;
+                document.getElementById("To").value = matched_trains.to_station;
+                document.getElementById("StartTime").value = matched_trains.starting_time;
+                document.getElementById("EndTime").value = matched_trains.reaching_time;
+
+
+
+        
+        }
+            
+
+    
+
 }
