2a3f4070d32e3e00ea2011dc59bfe8c27b68b0ab
Sadman Al Sakib <150785896+saadmanshakiib@users.noreply.github.com>
2025-06-07 05:21:16 +0600

    Add files via upload

---
+++ Bus Ticket/script.js
@@ -0,0 +1,189 @@
+        
+        let users = [
+        { 
+        Email : "saadman102002@gmail.com",
+        Pass : "asdf"
+        },
+    {
+        Email : "nissan120438@gmail.com",
+        Pass : "qwer"
+    }
+        ]
+
+function signup(){
+    window.location.href = "signupPage.html"
+}
+function signin(){
+    const email = document.getElementById("email").value;
+        const pass = document.getElementById("pass").value;
+
+            let isValid = false;
+
+        for(let i  = 0;i<users.length;i++){
+            if(users[i].Email == email && users[i].Pass == pass){
+                isValid = true;
+                    break;           
+                }
+            }
+
+            if(isValid){
+                window.location.href="homepage.html";
+            }
+
+            else{
+                alert("Incorrect Email or Password");
+                document.getElementById("email").value = "";
+                document.getElementById("pass").value = "";
+            }
+        }
+
+function check(){
+
+    const Password = document.getElementById("pass").value;
+    const ConfirmPass = document.getElementById("confirmedpass").value;
+    const enterEmail = document.getElementById("email").value;
+    const name = document.getElementById("name").value;
+    const number = document.getElementById("number").value;
+    const nid = document.getElementById("nid").value;
+    const date = document.getElementById("date").value;
+    const year = document.getElementById("year").value;
+
+    if(name == "" || number == "" || nid=="" || date == "" || year=="" || email =="" || Password=="" || ConfirmPass==""){
+        alert("Please fill all the required details");
+    }
+
+    else{
+
+    if(Password == ConfirmPass){
+            for(let i=0;i<users.length;i++){
+                if(users[i].Email == enterEmail){
+                    alert("Account Already exists");
+                    document.getElementById("pass").value = "";
+                    document.getElementById("confirmedpass").value = "";
+                    document.getElementById("name").value = "";
+                    document.getElementById("number").value = "";
+                    document.getElementById("email").value = "";
+                    document.getElementById("birthmont").value = "";
+
+                }
+                else if(users[i].Email != email){
+                    if(enterEmail.endsWith("@gmail.com") && number.length == 11 && year<=2025 && nid.length == 16 && date<=31){
+                        alert("Account has been created!");
+                        window.location.href="loginpage.html";
+                    }
+                    else{
+                        alert("Check all the fields correctly and fullfill");
+                    }
+                }
+                    }   
+            }
+    else{
+        alert("Passwords dont match");
+         document.getElementById("pass").value = "";
+                    document.getElementById("confirmedpass").value = "";
+                    document.getElementById("name").value = "";
+                    document.getElementById("number").value = "";
+                    document.getElementById("email").value = "";
+                    document.getElementById("birthmont").value = "";
+    }
+    }
+}
+
+function search(){
+    const from = document.getElementById("fromStation").value;
+    const to = document.getElementById("toStation").value;
+    const date = document.getElementById("journeyDate").value;
+
+    if(from == "" || to == "" || date == ""){
+        alert("Please fill all the fields");
+        return;
+    }
+    if(from == to){
+        alert("Both Starting Location & Destination Cant Be Same!");
+        return;
+    }
+
+                 let bus = [
+        {
+            bus_name : "Hanif",
+            bus_no : "701",
+            from_station : "Chittagong",
+            to_station : "Dhaka",
+            starting_time : "7:30 AM",
+            reaching_time : "12:00 PM",
+            boarding_point : "Dampara Bus Stand",
+            dropping_point : "Abdullahpur Bus Stand",
+                        break_point : "Hotel Noorzahan"
+        },
+        {
+            bus_name : "Sonar Bangla Express",
+            bus_no : "702",
+            from_station : "Chittagong",
+            to_station : "Dhaka",
+            starting_time : "5:00 PM",
+            reaching_time : "9:00 PM",
+            boarding_point : "Dampara Bus Stand",
+            dropping_point : "Abdullahpur Bus Stand",
+                        break_point : "Hotel Noorzahan"
+        },
+        {
+            bus_name : "Sonar Bangla Express",
+            bus_no : "703",
+            from_station : "Dhaka",
+            to_station : "Chittagong",
+            starting_time : "7:30 AM",
+            reaching_time : "12:00 PM",
+            boarding_point : "Abdullahpur Bus Stand",
+            dropping_point : "Dampara Bus Stand",
+                        break_point : "Hotel Noorzahan"
+        },
+        {
+            bus_name : "Hanif",
+            bus_no : "704",
+            from_station : "Dhaka",
+            to_station : "Chittagong",
+            starting_time : "7:30 AM",
+            reaching_time : "12:00 PM",
+            boarding_point : "Abdullahpur Bus Stand",
+            dropping_point : "Dampara Bus Stand",
+            break_point : "Hotel Noorzahan"
+        },
+        {
+            bus_name : "Saudia",
+            bus_no : "705",
+            from_station : "Chittagong",
+            to_station : "Dhaka",
+            starting_time : "10:00 AM",
+            reaching_time : "1:00 PM",
+            boarding_point : "Dampara Bus Stand",
+            dropping_point : "Abdullahpur Bus Stand",
+                        break_point : "Hotel Noorzahan"
+        },
+        {
+            bus_name : "Saudia",
+            bus_no : "706",
+            from_station : "Dhaka",
+            to_station : "Chittagong",
+            starting_time : "1:00 PM",
+            reaching_time : "6:00 PM",
+            boarding_point : "Abdullahpur Bus Stand",
+            dropping_point : "Dampara Bus Stand",
+                        break_point : "Hotel Noorzahan"
+        }
+    ]
+
+    let matched_bus = bus.filter(
+                                bus1 => bus1.from_station.toLowerCase() === from.toLowerCase() &&
+                                bus1.to_station.toLowerCase() == to.toLowerCase()
+                            );
+
+    localStorage.setItem("matched_bus",JSON.stringify(matched_bus));
+    localStorage.setItem("journeyDate",date);
+    window.location.href = "searchResults.html";
+}
+
+function checkSits(){
+    
+}
+        
+
