9dadcd1540404e01ed0be091eb71c3ecd24883c8
Sadman Al Sakib <150785896+saadmanshakiib@users.noreply.github.com>
2025-06-17 00:04:33 +0600

    Add files via upload

---
+++ Bus Ticket/Ticket/src/main/java/BusTicket/Ticket/Repo/UserRepository.java
@@ -0,0 +1,8 @@
+package BusTicket.Ticket.Repo;
+
+import BusTicket.Ticket.User.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
---
+++ Bus Ticket/Ticket/src/test/java/BusTicket/Ticket/TicketApplicationTests.java
@@ -0,0 +1,13 @@
+package BusTicket.Ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
